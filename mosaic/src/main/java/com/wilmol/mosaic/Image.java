3a2a0151acc6d71790fa8929ee26277bf3df9866
Will Molloy <willjoemolloy@gmail.com>
2021-05-29 18:18:26 +1200

    Get unique pixels

--- mosaic/src/main/java/com/wilmol/mosaic/Image.java
+++ mosaic/src/main/java/com/wilmol/mosaic/Image.java
@@ -11,6 +11,8 @@ import java.nio.file.Path;
 import java.util.List;
 import java.util.Objects;
 import java.util.Optional;
+import java.util.Set;
+import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import javax.annotation.concurrent.Immutable;
 import javax.annotation.concurrent.ThreadSafe;
@@ -195,5 +197,9 @@ final class Image {
   private static UncheckedIOException uncheckedIoException(String msg, IOException e) {
     log.error(msg, e);
     throw new UncheckedIOException(msg, e);
+  }
+
+  Set<RgbPixel> uniquePixels() {
+    return pixels().stream().flatMap(List::stream).collect(Collectors.toSet());
   }
 }
